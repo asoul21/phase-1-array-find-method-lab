f595307f58301808d67c55d323e3104afd5402fc
andrew soulinhavong <soulinhavong@gmail.com>
2024-01-12 05:08:23 -0500

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find(entry => entry.result === 'W');
+  
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+      return undefined;
+    }
+  }
